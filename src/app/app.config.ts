1e4e44f797b4064890b3985f6da812f8d7aac79a
mohammad0033 <mohammadebraheem3@gmail.com>
2025-04-26 13:40:22 +0300

    adjustments to config to handle translations and others

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,46 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {provideClientHydration, withEventReplay, withI18nSupport} from '@angular/platform-browser';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {GalleryModule} from 'ng-gallery';
+import {localizedApiInterceptor} from './core/interceptors/language.interceptor';
+import {API_PREFIX} from './core/tokens/API_PREFIX';
+import {Url} from './core/constants/base-url';
+
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
+  new TranslateHttpLoader(http, './i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay(),withI18nSupport()),
+    provideAnimations(),
+    provideAnimationsAsync(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: httpLoaderFactory,
+          deps: [HttpClient]
+        }
+      }),
+      GalleryModule
+    ),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([localizedApiInterceptor])
+    ),
+    {
+      provide: API_PREFIX,
+      useValue: Url.baseUrl // **Configure your API prefix here!**
+    },
+  ]
 };
