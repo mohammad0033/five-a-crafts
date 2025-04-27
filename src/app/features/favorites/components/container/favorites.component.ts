04ace3afc44a4696093cf703163a201ff8b1ac39
mohammad0033 <mohammadebraheem3@gmail.com>
2025-04-27 10:24:16 +0300

    initialized favorites

---
+++ src/app/features/favorites/components/container/favorites.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorites',
+  imports: [],
+  templateUrl: './favorites.component.html',
+  standalone: true,
+  styleUrl: './favorites.component.scss'
+})
+export class FavoritesComponent {
+
+}
