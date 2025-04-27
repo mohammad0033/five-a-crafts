f8e80f9265857405b3c1d8d435920ce2aa3b48e3
mohammad0033 <mohammadebraheem3@gmail.com>
2025-04-27 10:24:25 +0300

    initialized contact

---
+++ src/app/features/contact/components/container/contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  imports: [],
+  templateUrl: './contact.component.html',
+  standalone: true,
+  styleUrl: './contact.component.scss'
+})
+export class ContactComponent {
+
+}
