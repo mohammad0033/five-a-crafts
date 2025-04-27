41fd65b25a719fea5c0473e63cecace236d26a67
mohammad0033 <mohammadebraheem3@gmail.com>
2025-04-27 10:30:25 +0300

    initialized categories

---
+++ src/app/features/categories/components/container/categories.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories',
+  imports: [],
+  templateUrl: './categories.component.html',
+  standalone: true,
+  styleUrl: './categories.component.scss'
+})
+export class CategoriesComponent {
+
+}
