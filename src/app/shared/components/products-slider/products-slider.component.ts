2dfa6b2b24f4de735c9262c04d9a3ec9c7d373ec
mohammad0033 <mohammadebraheem3@gmail.com>
2025-04-29 14:50:47 +0300

    initialized products slider

---
+++ src/app/shared/components/products-slider/products-slider.component.ts
@@ -0,0 +1,57 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
+import {NgxSplideModule} from 'ngx-splide';
+import {NgForOf, NgIf} from '@angular/common';
+import {ProductCardComponent} from '../product-card/product-card.component';
+
+@UntilDestroy()
+@Component({
+  selector: 'app-products-slider',
+  imports: [
+    NgxSplideModule,
+    NgForOf,
+    NgIf,
+    ProductCardComponent
+  ],
+  templateUrl: './products-slider.component.html',
+  standalone: true,
+  styleUrl: './products-slider.component.scss'
+})
+export class ProductsSliderComponent implements OnInit{
+  @Input() products: any
+  currentLang!: string
+  splideOptions = {
+    height: '38rem',
+    perPage: 4,
+    perMove: 1,
+    gap: '1rem',
+    autoplay: false,
+    pagination: false,
+    breakpoints: {
+      1200: {
+        perPage: 3,
+      },
+      992: {
+        perPage: 2,
+      },
+      768: {
+        perPage: 1,
+      }
+    }
+  };
+
+  rtlSplideOptions = {
+    ...this.splideOptions,
+    direction: "rtl" as "rtl"
+  }
+
+  constructor(private translate: TranslateService) {}
+
+  ngOnInit() {
+    this.currentLang = this.translate.currentLang
+    this.translate.onLangChange.pipe(untilDestroyed(this)).subscribe((event: any) => {
+      this.currentLang = event.lang
+    })
+  }
+}
