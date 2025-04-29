0062538ed99ba797f2f9631cb8ac88ae0b08df76
mohammad0033 <mohammadebraheem3@gmail.com>
2025-04-29 14:56:12 +0300

    added our products component, adjusted html to be semantic to help seo

--- src/app/features/home/components/container/home.component.ts
+++ src/app/features/home/components/container/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {HeroCarouselComponent} from '../hero-carousel/hero-carousel.component';
+import {OurProductsComponent} from '../our-products/our-products.component';
 
 @Component({
   selector: 'app-home',
   imports: [
-    HeroCarouselComponent
+    HeroCarouselComponent,
+    OurProductsComponent
   ],
   templateUrl: './home.component.html',
   standalone: true,
