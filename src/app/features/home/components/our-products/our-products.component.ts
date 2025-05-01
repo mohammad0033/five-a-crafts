c1f3b3cf91d4b7dee5ecf276db6cc3d86ae85a1e
mohammad0033 <mohammadebraheem3@gmail.com>
2025-05-01 11:53:41 +0300

    adding finalize to handle loading

--- src/app/features/home/components/our-products/our-products.component.ts
+++ src/app/features/home/components/our-products/our-products.component.ts
@@ -5,6 +5,7 @@ import {NgIf} from '@angular/common';
 import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
 import {ProductsService} from '../../../../core/services/products.service';
 import {Product} from '../../../../core/models/product';
+import {finalize} from 'rxjs';
 
 @UntilDestroy()
 @Component({
@@ -29,18 +30,16 @@ export class OurProductsComponent implements OnInit {
     this.isLoading = true;
     this.productsService.getBestSellingProducts()
       .pipe(
-        untilDestroyed(this) // Use the untilDestroyed operator
+        untilDestroyed(this), // Use the untilDestroyed operator
+        finalize(() => (this.isLoading = false)) // Set isLoading to false when the observable completes
       )
       .subscribe({
         next: (data) => {
           this.products = data;
           console.log('Best selling products loaded:', this.products);
-          this.isLoading = false;
-          console.log('Best selling products loaded:', this.products);
         },
         error: (err) => {
           console.error('Error fetching best selling products:', err);
-          this.isLoading = false;
         }
       });
   }
