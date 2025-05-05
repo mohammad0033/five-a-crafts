2e73ff46facecce6f5db7ba5a730044f0b53d147
mohammad0033 <mohammadebraheem3@gmail.com>
2025-05-05 15:04:51 +0300

    moved template to a separate component

--- src/app/features/home/components/our-collections/our-collections.component.ts
+++ src/app/features/home/components/our-collections/our-collections.component.ts
@@ -1,39 +1,37 @@
 import {Component, OnInit} from '@angular/core';
-import {TranslatePipe, TranslateService} from '@ngx-translate/core';
-import {NgForOf, NgIf, SlicePipe} from '@angular/common';
+import {TranslateService} from '@ngx-translate/core';
 import {Product} from '../../../../core/models/product';
-import {ProductsService} from '../../../../core/services/products.service';
+import {ProductsApiService} from '../../../../core/services/products-api.service';
 import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
-import {ProductCardComponent} from '../../../../shared/components/product-card/product-card.component';
 import {faArrowLeft, faArrowRight} from '@fortawesome/free-solid-svg-icons';
-import {FaIconComponent} from '@fortawesome/angular-fontawesome';
-import {RouterLink} from '@angular/router';
-import {finalize} from 'rxjs';
+import {catchError, EMPTY, finalize, switchMap, tap, throwError} from 'rxjs';
+import {
+  CategoryCollectionComponent
+} from '../../../categories/components/category-collection/category-collection.component';
+import {Category} from '../../../../core/models/category';
+import {NgIf} from '@angular/common';
+import {CategoriesService} from '../../../categories/services/categories.service';
 
 @UntilDestroy()
 @Component({
   selector: 'app-our-collections',
   imports: [
-    TranslatePipe,
-    NgIf,
-    NgForOf,
-    ProductCardComponent,
-    SlicePipe,
-    FaIconComponent,
-    RouterLink
+    CategoryCollectionComponent,
+    NgIf
   ],
   templateUrl: './our-collections.component.html',
   standalone: true,
   styleUrl: './our-collections.component.scss'
 })
 export class OurCollectionsComponent implements OnInit {
+  category!: Category
   products : Product[] = []
   isLoading: boolean = false
   currentLang!: string
 
-  constructor(private productsService: ProductsService,
-              private translate: TranslateService) {
-  }
+  constructor(private categoriesService: CategoriesService,
+              private productsApiService: ProductsApiService,
+              private translate: TranslateService) {}
 
   ngOnInit() {
     this.currentLang = this.translate.currentLang
@@ -41,21 +39,56 @@ export class OurCollectionsComponent implements OnInit {
       this.currentLang = event.lang
     })
 
+    this.loadCollectionsData();
+  }
+
+  loadCollectionsData() {
     this.isLoading = true;
-    this.productsService.getCandlesCollectionProducts()
-      .pipe(
-        untilDestroyed(this), // Use the untilDestroyed operator
-        finalize(() => (this.isLoading = false)) // Set isLoading to false when the observable completes
-      )
-      .subscribe({
-        next: (data) => {
-          this.products = data;
-          console.log('Candles Collection products loaded:', this.products);
-        },
-        error: (err) => {
-          console.error('Error fetching candles collection products:', err);
+    this.categoriesService.getCategoriesData().pipe(
+      untilDestroyed(this),
+      tap(categories => {
+        // Find the 'Candles' category (adjust 'Candles' if the name might differ)
+        const candlesCategory = categories.find(cat => cat.name === 'Candles');
+        if (candlesCategory) {
+          this.category = candlesCategory;
+          console.log('Found Candles category:', this.category);
+        } else {
+          console.warn("Could not find 'Candles' category.");
+          // Optionally handle the case where the category isn't found
+          // Maybe throw an error or assign a default/empty category
+          throw new Error("Candles category not found"); // Or handle differently
         }
-      });
+      }),
+      // Use switchMap to switch to the products observable *after* categories are processed
+      switchMap(() => {
+        // Only fetch products if the category was found
+        if (this.category) {
+          return this.productsApiService.getCandlesCollectionProducts();
+        } else {
+          // If category wasn't found, return an empty observable or handle error
+          return EMPTY; // Or return throwError(() => new Error('Category not available'));
+        }
+      }),
+      // finalize will run when the *entire* chain completes or errors
+      finalize(() => {
+        this.isLoading = false;
+        console.log('Finished loading collections data.');
+      }),
+      // Catch errors from *either* API call
+      catchError(err => {
+        console.error('Error loading collections data:', err);
+        this.products = []; // Clear products on error
+        // Optionally, you could set an error message property to display in the template
+        return throwError(() => err); // Re-throw the error or return EMPTY/of([])
+      })
+    ).subscribe({
+      next: (productsData) => {
+        this.products = productsData;
+        console.log('Candles Collection products loaded:', this.products);
+      },
+      // Error handling is now primarily done in the catchError operator
+      // error: (err) => {} // This block is less necessary now but can be kept for specific final error actions
+    });
   }
 
   protected readonly faArrowLeft = faArrowLeft;
