8df507e62374f0f1cf8cfde9e292fed23dd0a46a
mohammad0033 <mohammadebraheem3@gmail.com>
2025-05-07 16:26:46 +0300

    importing hammerjs for gallery thumb and mouse swipe to work

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import 'hammerjs'; // for gallery thumb and mouse swipe to work
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
