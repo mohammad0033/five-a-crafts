1660ee5b65c3c85c119e47edef95d0eaad660de2
mohammad0033 <mohammadebraheem3@gmail.com>
2025-05-15 22:50:09 +0300

    initialized user info model

---
+++ src/app/features/profile/models/user-info.ts
@@ -0,0 +1,10 @@
+export interface UserInfo {
+  name: string;
+  company?: string;
+  address: string;
+  apartment?: string;
+  city: string;
+  governorate?: string;
+  phone: string;
+  email: string;
+}
