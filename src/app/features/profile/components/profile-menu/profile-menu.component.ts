e5bc2a678670d965517bb7d9ff6c96cb1b5e5fc0
mohammad0033 <mohammadebraheem3@gmail.com>
2025-05-15 22:49:17 +0300

    initialized profile menu component

---
+++ src/app/features/profile/components/profile-menu/profile-menu.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {faUser} from '@fortawesome/free-regular-svg-icons';
+import {TranslatePipe} from '@ngx-translate/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+import {faShoppingBag} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-profile-menu',
+  imports: [
+    FaIconComponent,
+    TranslatePipe,
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './profile-menu.component.html',
+  standalone: true,
+  styleUrl: './profile-menu.component.scss'
+})
+export class ProfileMenuComponent {
+
+  protected readonly faUser = faUser;
+  protected readonly faShoppingBag = faShoppingBag;
+}
