85d6bf8dfd54831bb06a9f68814ad0fe7914eab7
mohammad0033 <mohammadebraheem3@gmail.com>
2025-05-18 09:47:45 +0300

    an adjustment to make sure phone number is always ltr

--- src/app/features/profile/components/user-widget/user-widget.component.ts
+++ src/app/features/profile/components/user-widget/user-widget.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input} from '@angular/core';
-import {NgIf} from '@angular/common';
+import {Component, Input, OnInit} from '@angular/core';
+import {NgClass, NgIf} from '@angular/common';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 import {faUser} from '@fortawesome/free-regular-svg-icons';
+import {TranslateService} from '@ngx-translate/core';
 // import {TranslatePipe} from '@ngx-translate/core';
 // import {faEdit} from '@fortawesome/free-solid-svg-icons';
 
@@ -10,16 +11,28 @@ import {faUser} from '@fortawesome/free-regular-svg-icons';
   imports: [
     NgIf,
     FaIconComponent,
+    NgClass,
     // TranslatePipe
   ],
   templateUrl: './user-widget.component.html',
   standalone: true,
   styleUrl: './user-widget.component.scss'
 })
-export class UserWidgetComponent {
+export class UserWidgetComponent implements OnInit {
   @Input() image!:string
   @Input() name!:string
   @Input() phone!:string
   protected readonly faUser = faUser;
   // protected readonly faEdit = faEdit;
+  currentLang!: string
+
+  constructor(private translate: TranslateService) {
+  }
+
+  ngOnInit() {
+    this.currentLang = this.translate.currentLang;
+    this.translate.onLangChange.subscribe((event) => {
+      this.currentLang = event.lang;
+    });
+  }
 }
