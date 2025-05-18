e64c96fe110b01887966d8a3a7ddb57cea5f62f0
mohammad0033 <mohammadebraheem3@gmail.com>
2025-05-18 14:13:45 +0300

    changed the category model to match returned category from api

--- src/app/core/models/category.ts
+++ src/app/core/models/category.ts
@@ -1,6 +1,14 @@
 export interface Category {
   id: number;
+  url: string;
+  breadcrumbs: string;
+  children: string;
   name: string;
-  description?: string;
+  description: string;
   image: string;
+  meta_title: string;
+  meta_description: string;
+  get_num_children: number;
+  childrens: any[];
+  slug: string;
 }
