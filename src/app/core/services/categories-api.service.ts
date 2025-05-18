93831feb0080301d4c5e8db482ee8f20ee5cc139
mohammad0033 <mohammadebraheem3@gmail.com>
2025-05-18 13:43:13 +0300

    added correct live api

--- src/app/core/services/categories-api.service.ts
+++ src/app/core/services/categories-api.service.ts
@@ -1,46 +1,17 @@
 import { Injectable } from '@angular/core';
-import {delay, Observable, of} from 'rxjs';
-import {Category} from '../models/category';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {Url} from '../constants/base-url';
+import {CommonApiResponse} from '../models/common-api-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  getCategoriesData(): Observable<any[]> {
-
-    const mockCategoriesData: Category[] = [
-      {
-        id: 1,
-        name: 'Candles',
-        description: 'Discover our potent unique candles collection.',
-        image: 'https://picsum.photos/id/1063/300/300'
-      },
-      {
-        id: 2,
-        name: 'Decor',
-        description: 'Discover our unique home decor collection.',
-        image: 'https://picsum.photos/id/1064/300/300'
-      },
-      {
-        id: 3,
-        name: 'Ramadan',
-        description: 'Discover our unique Ramadan collection.',
-        image: 'https://picsum.photos/id/1065/300/300'
-      },
-      {
-        id: 4,
-        name: 'Gifts',
-        description: 'Discover our unique gifts collection.',
-        image: 'https://picsum.photos/id/1066/300/300'
-      }
-    ]
-
-    // Use `of` and `delay` to simulate a network request
-    // In a real scenario, this would be an HttpClient call:
-    // return this.http.get<CategoriesItem[]>('/api/carousel-data');
-    return of(mockCategoriesData).pipe(delay(1000)); // Simulate 500ms delay
+  getCategoriesData():Observable<CommonApiResponse>{
+    return this.http.get<CommonApiResponse>(`${Url.baseUrl}/api/oscar/categories/`);
   }
 }
