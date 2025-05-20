2132a4005085f3ccf915da2665fc778bb87f2d03
mohammad0033 <mohammadebraheem3@gmail.com>
2025-05-20 15:31:18 +0300

    an adjustment to make sure on router navigation end event scroll to the top of the current view

--- src/app/core/components/container/container.component.ts
+++ src/app/core/components/container/container.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterModule} from '@angular/router';
 import {FooterComponent} from '../footer/footer.component';
 import {NavbarComponent} from '../navbar/navbar.component';
-import {CommonModule} from '@angular/common';
+import {CommonModule, isPlatformBrowser} from '@angular/common';
 import {filter, Observable} from 'rxjs';
 import {MatProgressBar} from '@angular/material/progress-bar';
 import {CartService} from '../../services/cart.service';
@@ -32,10 +32,13 @@ export class ContainerComponent implements OnInit{
   // Example value for determinate mode (0-100)
   loadingProgress = 50;
   isCartDrawerOpen$!: Observable<boolean>;
+  private isBrowser: boolean;
 
   constructor(private router: Router,
               private cartService: CartService,
-              private favoriteService: FavoritesApiService) {
+              private favoriteService: FavoritesApiService,
+              @Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
     this.isCartDrawerOpen$ = this.cartService.drawerOpen$;
     this.router.events.pipe(
       // Filter for the relevant navigation events
@@ -50,8 +53,19 @@ export class ContainerComponent implements OnInit{
         this.isLoading = true;
         // Reset progress if using determinate (though usually indeterminate is better for resolvers)
         this.loadingProgress = 0;
+      } else if (event instanceof NavigationEnd) {
+        this.isLoading = false;
+        // Scroll to top on navigation complete
+        if (this.isBrowser) {
+          setTimeout(() => {
+            const scrollableContent = document.querySelector('.mat-drawer-content');
+            if (scrollableContent) {
+              scrollableContent.scrollTo({ top: 0, behavior: 'smooth' });
+            }
+          }, 0); // 0ms or a slightly higher value like 50-100ms if needed
+        }
       } else {
-        // NavigationEnd, NavigationCancel, NavigationError
+        // NavigationCancel, NavigationError
         this.isLoading = false;
       }
     });
