dba6e9c64b4d99aab7fa92d83d54ff7aa14ea10e
mohammad0033 <mohammadebraheem3@gmail.com>
2025-05-20 11:59:29 +0300

    adjusted model properties

--- src/app/core/models/common-api-response.ts
+++ src/app/core/models/common-api-response.ts
@@ -1,5 +1,8 @@
+import {PaginatedApiObject} from './paginated-api-object';
+
 export interface CommonApiResponse {
   status: boolean;
+  filters?: {};
   message: string;
-  data: any;
+  data: {} | any[] | PaginatedApiObject;
 }
