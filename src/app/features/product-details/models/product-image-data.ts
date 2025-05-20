5aa314732f3c700c40e65585ded35e13132633a8
mohammad0033 <mohammadebraheem3@gmail.com>
2025-05-20 11:38:10 +0300

    added properties to model

--- src/app/features/product-details/models/product-image-data.ts
+++ src/app/features/product-details/models/product-image-data.ts
@@ -1,4 +1,9 @@
 export interface ProductImageData {
-  url: string;
-  altText?: string;
+  id: string | number;
+  code: string;
+  original: string;
+  caption: string;
+  display_order: number;
+  date_created: string;
+  url?: string;
 }
