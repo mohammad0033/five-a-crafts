761c8f541b24181eb9b3bbd54f42eb3068e1aec2
mohammad0033 <mohammadebraheem3@gmail.com>
2025-05-20 11:43:18 +0300

    adjusted model properties

--- src/app/core/models/product.ts
+++ src/app/core/models/product.ts
@@ -1,9 +1,19 @@
+import {ProductImageData} from '../../features/product-details/models/product-image-data';
+import {Price} from './price';
+
 export interface Product {
+  url?: string;
   id: string | number;
-  name: string;
-  description?: string;
-  imageUrl: string;
-  price: number;
-  isFavorite?: boolean;
-  altText?: string; // Optional alt text for images
+  title: string;
+  description: string;
+  images?: ProductImageData[];
+  product_class?: string;
+  price?: Price;
+  stock?: number;
+  slug?: string;
+  calculate_rating?: any;
+  num_approved_reviews?: number;
+  rating?: number;
+  has_options?: boolean;
+  in_wishlist: boolean;
 }
