4af7114a24f4af16b86b1df7fd02e84df1118c02
mohammad0033 <mohammadebraheem3@gmail.com>
2025-05-20 11:51:05 +0300

    our products now fetch data from home service not from products api service directly

--- src/app/features/home/components/our-products/our-products.component.ts
+++ src/app/features/home/components/our-products/our-products.component.ts
@@ -3,7 +3,6 @@ import {TranslatePipe, TranslateService} from '@ngx-translate/core';
 import {ProductsSliderComponent} from '../../../../shared/components/products-slider/products-slider.component';
 import {NgForOf, NgIf, SlicePipe} from '@angular/common';
 import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
-import {ProductsApiService} from '../../../../core/services/products-api.service';
 import {Product} from '../../../../core/models/product';
 import {finalize} from 'rxjs';
 import {ProductCardComponent} from '../../../../shared/components/product-card/product-card.component';
@@ -11,6 +10,7 @@ import {RouterLink} from '@angular/router';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 import {faArrowLeft, faArrowRight} from '@fortawesome/free-solid-svg-icons';
 import {FavoritesApiService} from '../../../../core/services/favorites-api.service';
+import {HomeService} from '../../services/home.service';
 
 @UntilDestroy()
 @Component({
@@ -37,7 +37,7 @@ export class OurProductsComponent implements OnInit {
   protected readonly faArrowRight = faArrowRight;
   protected readonly faArrowLeft = faArrowLeft;
 
-  constructor(private productsApiService: ProductsApiService,
+  constructor(private homeService: HomeService,
               private favoritesApiService: FavoritesApiService,
               private translate: TranslateService) {
   }
@@ -49,13 +49,14 @@ export class OurProductsComponent implements OnInit {
       this.currentLang = event.lang
     })
     this.isLoading = true;
-    this.productsApiService.getBestSellingProducts()
+    this.homeService.getBestSellingProducts()
       .pipe(
         untilDestroyed(this), // Use the untilDestroyed operator
         finalize(() => (this.isLoading = false)) // Set isLoading to false when the observable completes
       )
       .subscribe({
         next: (data) => {
+          console.log('Best selling products data:', data);
           this.products = data;
           console.log('Best selling products loaded:', this.products);
         },
@@ -66,21 +67,21 @@ export class OurProductsComponent implements OnInit {
   }
 
   handleFavoriteToggle(productToToggle: Product): void {
-    console.log(`Component: Toggling favorite for ${productToToggle.name}`);
+    console.log(`Component: Toggling favorite for ${productToToggle.title}`);
     this.favoritesApiService.toggleFavorite(productToToggle.id)
       .pipe(untilDestroyed(this)) // Component subscribes to the toggle action
       .subscribe({
         next: (result) => {
           if (result.action === 'added' && result.product) {
-            console.log(`${result.product.name} was added to favorites. List will refresh via service.`);
+            console.log(`${result.product.title} was added to favorites. List will refresh via service.`);
           } else if (result.action === 'removed') {
-            // Using productToToggle.name here as the removed product object isn't always returned by remove ops
-            console.log(`${productToToggle.name} (ID: ${result.productId}) was removed from favorites. List will refresh via service.`);
+            // Using productToToggle.title here as the removed product object isn't always returned by remove ops
+            console.log(`${productToToggle.title} (ID: ${result.productId}) was removed from favorites. List will refresh via service.`);
           }
           // The favorites list (products$) will update automatically because
           // toggleFavorite calls addFavorite/removeFavorite, which in turn call loadFavorites.
         },
-        error: (err) => console.error(`Component: Failed to toggle favorite for ${productToToggle.name}`, err)
+        error: (err) => console.error(`Component: Failed to toggle favorite for ${productToToggle.title}`, err)
       });
   }
 }
