5cf4a02448022b444bef56b757b3dd1cf2149b62
mohammad0033 <mohammadebraheem3@gmail.com>
2025-05-21 11:08:01 +0300

    adjusted model properties

--- src/app/features/cart/models/cart-item.ts
+++ src/app/features/cart/models/cart-item.ts
@@ -1,9 +1,11 @@
 import {Product} from '../../../core/models/product';
+import {SelectedVariation} from './selected-variation';
 
 export interface CartItem {
+  id: string; // A unique ID for this cart item instance (product.id + serialized_variations)
   product: Product;       // The core product details (id, name, price, imageUrl)
   quantity: number;       // The quantity selected by the user (from productDetailsComponent.productQty)
   originalStock?: number; // The stock quantity of the product (from productDetailsData.stockQuantity)
   // This is useful for the quantity controls within the cart item itself.
-  // variations?: any;    // Any selected product variations
+  selectedVariations?: SelectedVariation; // To store chosen options like color
 }
