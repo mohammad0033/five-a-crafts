c7a12c09a28a259a774d68b70e523dbceae8d899
mohammad0033 <mohammadebraheem3@gmail.com>
2025-05-25 14:49:25 +0300

    adjusted search widget view

--- src/app/features/products/components/search-widget/search-widget.component.ts
+++ src/app/features/products/components/search-widget/search-widget.component.ts
@@ -2,23 +2,23 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {TranslatePipe} from '@ngx-translate/core';
 import {FormControl, ReactiveFormsModule} from '@angular/forms';
 import {debounceTime, distinctUntilChanged, filter} from 'rxjs';
-import {MatFormField, MatLabel} from '@angular/material/form-field';
-import {MatInput} from '@angular/material/input';
-import {MatIconButton} from '@angular/material/button';
-import {NgIf} from '@angular/common';
-import {MatIcon} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+// import {NgIf} from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+// import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search-widget',
   imports: [
-    TranslatePipe,
-    MatFormField,
-    MatInput,
     ReactiveFormsModule,
-    MatIconButton,
-    NgIf,
-    MatIcon,
-    MatLabel
+    // NgIf,
+    TranslatePipe,
+    // FaIconComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ],
   templateUrl: './search-widget.component.html',
   standalone: true,
@@ -68,4 +68,6 @@ export class SearchWidgetComponent implements OnInit {
     this.searchControl.setValue('');
     // The valueChanges subscription will emit an empty string due to the updated filter logic.
   }
+
+  protected readonly faSearch = faSearch;
 }
