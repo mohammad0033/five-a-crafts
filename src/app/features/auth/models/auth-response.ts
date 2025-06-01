e34925ac3cf6717d4c7dff7d8fb2cd6569cf437f
mohammad0033 <mohammadebraheem3@gmail.com>
2025-06-01 11:49:57 +0300

    adjusted model

--- src/app/features/auth/models/auth-response.ts
+++ src/app/features/auth/models/auth-response.ts
@@ -1,7 +1,8 @@
 import {AuthData} from './auth-data';
+import {SuccessRegister} from './success-register';
 
 export interface AuthResponse {
   status: boolean;
   message: string;
-  data?: AuthData; // Make data optional in case of error responses
+  data?: AuthData | SuccessRegister; // Make data optional in case of error responses
 }
