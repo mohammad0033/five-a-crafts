abc6437d636d8f71e5e01980da068f84e48dd521
mohammad0033 <mohammadebraheem3@gmail.com>
2025-06-02 14:18:52 +0300

    added summary calculation function that gets reviews data and returns reviews summary, added a function to prompt the user to log in before adding a review

--- src/app/core/services/products.service.ts
+++ src/app/core/services/products.service.ts
@@ -1,6 +1,6 @@
 import {inject, Injectable} from '@angular/core';
 import {ProductsApiService} from './products-api.service';
-import {catchError, map, Observable, of, switchMap, tap, throwError} from 'rxjs';
+import {catchError, map, Observable, of, switchMap, throwError} from 'rxjs';
 import {Product} from '../models/product';
 import {PaginatedApiObject} from '../models/paginated-api-object';
 import {CommonApiResponse} from '../models/common-api-response';
